ce577bf8776ad709dba4188b5cce70968751dc55
nabih uzcategui <nabihuzcateguip@gmail.com>
2023-04-23 18:19:50 -0300

    23-04-23/color_actualizacion

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 
 const kBottomContainerHeght = 80.0;
-const Color kActiveCardColor = Color(0xff323232);
-// Color kActiveCardColor = Colors.teal.shade600;
+const Color kActiveCardColor = Color(0xff404040);
 const Color kInActiveCardColor = Color(0xff242424);
-// const Color kBottomContainerColors = Color(0xffeb1555);
- Color kBottomContainerColors = Colors.pink.shade700;
-// const Color kSliderActiveColor = Color(0xffeb1555);
- Color kSliderActiveColor = Colors.pink.shade700;
+ Color kBottomContainerColors = Colors.teal.shade700;
+ Color kSliderActiveColor = Colors.teal.shade700;
 const Color kSliderInactiveColor = Color(0xff242424);
-// const Color kFloatingActionButtonColor = Color(0xffeb1555);
-Color kFloatingActionButtonColor = Colors.pink.shade700;
+Color kFloatingActionButtonColor = Colors.teal.shade700;
 
 const kLabelTextStyle = TextStyle(color: Color(0xff8d8e98), fontSize: 18.0);
 
@@ -20,7 +16,7 @@ const kLargeButtonTextStyle = TextStyle( fontSize: 30.0, fontWeight: FontWeight.
 
 const kTitleTextResultStyle = TextStyle( fontSize: 50.0, fontWeight: FontWeight.bold);
 
-const kResultsTextStyle = TextStyle( fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.green);
+const kResultsTextStyle = TextStyle( fontSize: 30.0, fontWeight: FontWeight.bold, color: Colors.green);
 
 const kBMITextStyle = TextStyle( fontSize: 100.0, fontWeight: FontWeight.bold);
 
